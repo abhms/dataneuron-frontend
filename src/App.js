1eb549503974f963720026a70fa125a52f635705
Abhishek Mishra <65820065+abhms@users.noreply.github.com>
2024-03-19 12:27:21 +0530

    get and update api implemented and integrated on frontend

--- src/App.js
+++ src/App.js
@@ -9,40 +9,69 @@ import Modal from "./modal/modal";
 function App() {
   // const dispatch = useDispatch();
   const [data, setData] = useState(null);
+  const [executiontime, setExecutionTime] = useState();
+  const [componentExecutionTime, setcomponentExecutionTime] = useState();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [action, setAction] = useState("");
-  const openModal = () => {
+  const [modalState, setModalState] = useState({
+    1: { isOpen: false, action: "" },
+    2: { isOpen: false, action: "" },
+    3: { isOpen: false, action: "" },
+  });
+  // const openModal = () => {
+  //   setIsModalOpen(true);
+  //   setAction("add");
+  // };
+  const updateModal = () => {
     setIsModalOpen(true);
-    setAction("add")
+    setAction("update");
   };
-const updateModal=()=>{
-  setIsModalOpen(true);
-  setAction("update")
-}
-console.log(action,"action")
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
+  console.log(action, "action");
+  // const closeModal = () => {
+  //   setIsModalOpen(false);
+  // };
   useEffect(() => {
     fetchData();
   }, []);
 
-  
+  const openModal = (number, action) => {
+    setModalState((prevState) => ({
+      ...prevState,
+      [number]: { isOpen: true, action },
+    }));
+  };
+
+  const closeModal = (number) => {
+    setModalState((prevState) => ({
+      ...prevState,
+      [number]: { isOpen: false, action: "" },
+    }));
+  };
+
   const fetchData = async () => {
     try {
       const response = await axios.get(
         `${process.env.REACT_APP_BACKEND_URL}/allData`
       );
       setData(response.data.allData);
-      console.log(response, "ppppppp");
+      setExecutionTime(response?.data?.executionTime);
+      console.log(response.data, "ppppppp");
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
 
-  console.log(data, "pppp");
+  console.log(data, "data");
   return (
     <div className="App">
+      <h2>
+        Execution Time of API: ~{executiontime && executiontime.toFixed(2)}
+      </h2>
+      <h2>
+       Round of Execution time of Add or Update API : 
+        {componentExecutionTime ? componentExecutionTime.toFixed(2) : " ---"}
+      </h2>
+
       <div className="top-row">
         <Resizable
           className="my-element"
@@ -65,16 +94,33 @@ console.log(action,"action")
             <strong>API Call:</strong> {data?.comp1Data[0]?.apiCall}{" "}
             <strong>API Update:</strong> {data?.comp1Data[0]?.apiupdate}
             <div className="buttons">
-              <button onClick={openModal} className="add-button">
+              <button
+                onClick={() => {
+                  openModal(1, "add");
+                  setAction("add");
+                }}
+                className="add-button"
+              >
                 Add
               </button>
-              <button onClick={updateModal}  className="update-button">Update</button>
+
+              <button
+                onClick={() => {
+                  openModal(1, "update");
+                  setAction("update");
+                }}
+                className="update-button"
+              >
+                Update
+              </button>
             </div>
             <Modal
-              isOpen={isModalOpen}
-              onClose={closeModal}
+              isOpen={modalState[1].isOpen}
+              onClose={() => closeModal(1)}
               number={1}
               action={action}
+              setData={setData}
+              setcomponentExecutionTime={setcomponentExecutionTime}
               firstname={data?.comp1Data[0]?.firstname}
               lastname={data?.comp1Data[0]?.lastname}
               email={data?.comp1Data[0]?.email}
@@ -102,15 +148,33 @@ console.log(action,"action")
             <strong>API Call:</strong> {data?.comp2Data[0]?.apiCall}{" "}
             <strong>API Update:</strong> {data?.comp2Data[0]?.apiupdate}
             <div className="buttons">
-              <button onClick={openModal} className="add-button">Add</button>
-              <button onClick={updateModal}  className="update-button">Update</button>
+              <button
+                onClick={() => {
+                  openModal(2, "add");
+                  setAction("add");
+                }}
+                className="add-button"
+              >
+                Add
+              </button>
+              <button
+                onClick={() => {
+                  openModal(2, "update");
+                  setAction("update");
+                }}
+                className="update-button"
+              >
+                Update
+              </button>
             </div>
             <Modal
-              isOpen={isModalOpen}
-              onClose={closeModal}
+              isOpen={modalState[2].isOpen}
+              onClose={() => closeModal(2)}
               number={2}
               action={action}
+              setData={setData}
 
+              setcomponentExecutionTime={setcomponentExecutionTime}
               firstname={data?.comp2Data[0]?.firstname}
               lastname={data?.comp2Data[0]?.lastname}
               email={data?.comp2Data[0]?.email}
@@ -140,15 +204,32 @@ console.log(action,"action")
             <strong>API Call:</strong> {data?.comp3Data[0]?.apiCall}{" "}
             <strong>API Update:</strong> {data?.comp3Data[0]?.apiupdate}
             <div className="buttons">
-              <button onClick={openModal} className="add-button">Add</button>
-              <button onClick={updateModal}  className="update-button">Update</button>
+              <button
+                onClick={() => {
+                  openModal(3, "add");
+                  setAction("add");
+                }}
+                className="add-button"
+              >
+                Add
+              </button>
+              <button
+                onClick={() => {
+                  openModal(3, "update");
+                  setAction("update");
+                }}
+                className="update-button"
+              >
+                Update
+              </button>
             </div>
             <Modal
-              isOpen={isModalOpen}
-              onClose={closeModal}
+              isOpen={modalState[3].isOpen}
+              onClose={() => closeModal(3)}
               number={3}
               action={action}
-
+              setData={setData}
+              setcomponentExecutionTime={setcomponentExecutionTime}
               firstname={data?.comp3Data[0]?.firstname}
               lastname={data?.comp3Data[0]?.lastname}
               email={data?.comp3Data[0]?.email}
--- src/modal/modal.js
+++ src/modal/modal.js
@@ -1,10 +1,165 @@
-import React, { useState } from "react";
-
-const Modal = ({ isOpen, onClose, email, firstname, lastname, action, number }) => {
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+const Modal = ({
+  isOpen,
+  onClose,
+  email,
+  firstname,
+  lastname,
+  action,
+  number,
+  setData,
+  setcomponentExecutionTime,
+}) => {
+  console.log(
+    email,
+    firstname,
+    lastname,
+    action,
+    number,
+    "email, firstname, lastname, action, number"
+  );
   const [editedEmail, setEditedEmail] = useState(email);
   const [editedFirstname, setEditedFirstname] = useState(firstname);
   const [editedLastname, setEditedLastname] = useState(lastname);
-  const [editedNumber, setEditedNumber] = useState(number);
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_BACKEND_URL}/allData`
+      );
+      setData(response.data.allData);
+      setcomponentExecutionTime(response?.data?.executionTime);
+
+      console.log(response, "ppppppp");
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  const add1 = async () => {
+    if (action == "add") {
+      try {
+        const response = await axios.post(
+          `${process.env.REACT_APP_BACKEND_URL}/component1`,
+          {
+            email: editedEmail,
+            firstname: editedFirstname,
+            lastname: editedLastname,
+          }
+        );
+        setcomponentExecutionTime(response?.data?.executionTime);
+        await fetchData().then(() => {
+          onClose();
+        });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    } else {
+      try {
+        const response = await axios.post(
+          `${process.env.REACT_APP_BACKEND_URL}/updateComp1`,
+          {
+            email: editedEmail,
+            firstname: editedFirstname,
+            lastname: editedLastname,
+          }
+        );
+        setcomponentExecutionTime(response?.data?.executionTime);
+
+        await fetchData().then(() => {
+          onClose();
+        });
+
+        console.log(response, "ppppppp");
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+  };
+  
+  const add2 = async () => {
+    if (action == "add") {
+      try {
+        const response = await axios.post(
+          `${process.env.REACT_APP_BACKEND_URL}/component2`,
+          {
+            email: editedEmail,
+            firstname: editedFirstname,
+            lastname: editedLastname,
+          }
+        );
+        setcomponentExecutionTime(response?.data?.executionTime);
+
+        await fetchData().then(() => {
+          onClose();
+        });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    } else {
+      try {
+        const response = await axios.post(
+          `${process.env.REACT_APP_BACKEND_URL}/updateComp2`,
+          {
+            email: editedEmail,
+            firstname: editedFirstname,
+            lastname: editedLastname,
+          }
+        );
+        setcomponentExecutionTime(response?.data?.executionTime);
+
+        await fetchData().then(() => {
+          onClose();
+        });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+  };
+  const add3 = async () => {
+    if (action == "add") {
+      try {
+        const response = await axios.post(
+          `${process.env.REACT_APP_BACKEND_URL}/component3`,
+          {
+            email: editedEmail,
+            firstname: editedFirstname,
+            lastname: editedLastname,
+          }
+        );
+        setcomponentExecutionTime(response?.data?.executionTime);
+
+        await fetchData().then(() => {
+          onClose();
+        });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    } else {
+      try {
+        const response = await axios.post(
+          `${process.env.REACT_APP_BACKEND_URL}/updateComp3`,
+          {
+            email: editedEmail,
+            firstname: editedFirstname,
+            lastname: editedLastname,
+          }
+        );
+        setcomponentExecutionTime(response?.data?.executionTime);
+
+        await fetchData().then(() => {
+          onClose();
+        });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    setEditedEmail(email);
+    setEditedFirstname(firstname);
+    setEditedLastname(lastname);
+  }, [email, firstname, lastname, number]);
 
   const handleEmailChange = (event) => {
     setEditedEmail(event.target.value);
@@ -18,25 +173,60 @@ const Modal = ({ isOpen, onClose, email, firstname, lastname, action, number }) 
     setEditedLastname(event.target.value);
   };
 
-  const handleNumberChange = (event) => {
-    setEditedNumber(event.target.value);
-  };
-
   if (!isOpen) return null;
-
+  console.log(editedEmail, "editedEmail");
   return (
-    <div className="modal">
-      <h1>{action}</h1>
-      <div className="modal-content">
-        <span className="close" onClick={onClose}>
-          &times;
-        </span>
-        <input value={editedEmail} onChange={handleEmailChange} />
-        <input value={editedFirstname} onChange={handleFirstnameChange} />
-        <input value={editedLastname} onChange={handleLastnameChange} />
-        <input value={editedNumber} onChange={handleNumberChange} />
-        <br/>
-        <button>{action}</button>
+    <div className="modal-overlay">
+      <div className="modal">
+        <div className="modal-header">
+          <span className="close" onClick={onClose}>
+            &times;
+          </span>
+        </div>
+        <div className="modal-content">
+        <h1 style={{alignContent:"center"}}>{action && action=="add"?"Add":"Update"}</h1>
+
+          <label>Email:</label>
+          <input
+            value={editedEmail}
+            onChange={handleEmailChange}
+            type="email"
+            placeholder="Enter your email"
+          />
+          <br />
+          <label>First Name:</label>
+          <input
+            value={editedFirstname}
+            onChange={handleFirstnameChange}
+            type="text"
+            placeholder="Enter your first name"
+          />
+          <label>Last Name:</label>
+          <input
+            value={editedLastname}
+            onChange={handleLastnameChange}
+            type="text"
+            placeholder="Enter your last name"
+          />
+          <div className="modal-footer">
+            <button onClick={onClose} style={{ backgroundColor: "red" }}>
+              Close
+            </button>
+            <button
+              onClick={() => {
+                if (number && number === 1) {
+                  add1();
+                } else if (number && number === 2) {
+                  add2();
+                } else {
+                  add3();
+                }
+              }}
+            >
+              {action}
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
